879414968c6ad9e8bd351343c27c0322954aa06d
Michael Pfeifer <mikepf97@gmail.com>
2015-10-09 10:29:33 -0600

    Made menu

---
+++ Palindrome/prog3.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+bool   recognizer       (string palindrome);
+string decorruptionator (string palindrome);
+bool   detective        (string palindrome);
+string cipher           (string palindrome);
+string decipher         (string palindrome);
+
+string toStandard       (string palindrome);
+
+int main() {
+    bool quit = false;
+    char choice;
+    do {
+        cout << "Welcome to Palindromes-R-Us!" << endl;
+        cout << "Select an option" << endl;
+        cout << endl;
+        cout << "1. Palindrome Recognizer" << endl;
+        cout << "2. Palindrome Decorruptionator" << endl;
+        cout << "3. Palindrome Detective" << endl;
+        cout << "4. Palindrome Cipher" << endl;
+        cout << "5. Palindrome Decipher" << endl;
+        cout << "6. Quit" << endl;
+        cout << endl;
+
+        cout << "Enter choice: ";
+        cin >> choice;
+
+        switch(choice) {
+            case '1':
+                cout << "Entered 1" << endl;
+                break;
+            case '2':
+                cout << "Entered 2" << endl;
+                break;
+            case '3':
+                cout << "Entered 3" << endl;
+                break;
+            case '4':
+                cout << "Entered 4" << endl;
+                break;
+            case '5':
+                cout << "Entered 5" << endl;
+                break;
+            case '6':
+                cout << "Goodbye!" << endl;
+                quit = true;
+                break;
+        }
+
+        cout << endl;
+    } while(!quit);
+}
