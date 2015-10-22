e776cc59300b2940885deef363a66cbbfc449f2e
Michael Pfeifer <mikepf97@gmail.com>
2015-10-22 10:49:48 -0600

    Started converting to c string

--- Palindrome/prog3.cpp
+++ Palindrome/prog3.cpp
@@ -46,6 +46,7 @@
    Oct 11, 2015  Finished decorruptionator, started detective
    Oct 12, 2015  Completed detective, debugged more methods
    Oct 13, 2015  Completed project, added Doxygen documentation
+   Oct 22, 2015  Converted everthing to c-style stings. Thanks for that BTW
    @endverbatim
  *
  *****************************************************************************/
@@ -54,22 +55,22 @@
 // Preprocessor
 #include <algorithm>
 #include <iostream>
-#include <string>
+#include <cctype>
 
 using namespace std;
 
 // Prototypes
-bool   recognizer        (string palindrome);
-string decorruptionator  (string palindrome);
-bool   detective         (string palindrome);
-string cipher            (string palindrome);
-string decipher          (string palindrome);
+bool recognizer         (char palindrome[]);
+void decorruptionator   (char palindrome[]);
+bool detective          (char palindrome[]);
+void cipher             (char palindrome[]);
+void decipher           (char palindrome[]);
 
-string toStandard        (string palindrome);
-string getInput          ();
+void toStandard         (char palindrome[]);
+void getInput           (char palindrome[]);
 
-string getNextLetter     (char letter);
-string getPreviousLetter (char letter);
+char getNextLetter      (char letter);
+char getPreviousLetter  (char letter);
 
 
 /**************************************************************************//**
@@ -85,7 +86,7 @@ int main() {
     // Quit will keep the program looping until the user wants to quit
     bool quit = false;
     char choice;
-    string phrase;
+    char phrase[81] = {'\0'};
 
     do {
         // Output main menu
@@ -108,7 +109,7 @@ int main() {
         switch(choice) {
             case '1': // Recognizer
                 cout << "Enter a string: ";
-                phrase = getInput();
+                getInput(phrase);
                 if(recognizer(phrase)) {
                     cout << phrase << " is a palindrome" << endl;
 
@@ -118,18 +119,21 @@ int main() {
                 break;
             case '2': // Decorruptionator
                 cout << "Enter a string: ";
-                phrase = getInput();
+                getInput(phrase);
                 if(recognizer(phrase)) {
                     cout << phrase << " is already a palindrome" << endl;
-                } else if(decorruptionator(phrase) != "") {
-                    cout << "The decorrupted palindrome is " << decorruptionator(phrase) << endl;
                 } else {
-                    cout << "This palindrome can't be decorrupted" << endl;;
+                    decorruptionator(phrase);
+                    if(phrase != "") {
+                        cout << "The decorrupted palindrome is " << phrase << endl;
+                    } else {
+                        cout << "This palindrome can't be decorrupted" << endl;
+                    }
                 }
                 break;
             case '3': // Detective
                 cout << "Enter a string: ";
-                phrase = getInput();
+                getInput(phrase);
                 if(detective(phrase)) {
                     cout << "This can be a palindrome" << endl;
                 } else {
@@ -138,17 +142,20 @@ int main() {
                 break;
             case '4': // Cipher
                 cout << "Enter a string: ";
-                phrase = getInput();
+                getInput(phrase);
                 if(recognizer(phrase)) {
-                    cout << "Palindrome cipher complete: " << cipher(toStandard(phrase)) << endl;
+                    toStandard(phrase);
+                    cout << "Palindrome cipher complete: " << cipher(phrase) << endl;
                 } else {
                     cout << phrase << " is not a palindrome" << endl;
                 }
                 break;
             case '5': // Decipher
                 cout << "Enter a string: ";
-                phrase = getInput();
-                cout << "Palindromic decipher complete: " << decipher(toStandard(phrase)) << endl;
+                getInput(phrase);
+                toStandard(phrase);
+                decipher(phrase);
+                cout << "Palindromic decipher complete: " << phrase << endl;
                 break;
 
                 break;
@@ -177,7 +184,7 @@ int main() {
  * @returns     false       - the input was not a palindrome
  *
  *****************************************************************************/
-bool recognizer(string palindrome) {
+bool recognizer(char palindrome[]) {
     palindrome = toStandard(palindrome);
     for(int i = 0; i < palindrome.length() / 2; i++) {
         // Tests if first equals last, 2nd equals 2nd to last, etc...
@@ -201,15 +208,16 @@ bool recognizer(string palindrome) {
  * @returns     [string]    - the input can be a palindrome, string contains a potential palindrome
  *
  *****************************************************************************/
-string decorruptionator(string palindrome) {
-    string original = palindrome; // So that palindrome.erase doesn't wreck the origonal copy
+void decorruptionator(char palindrome[]) {
+    char[] original = palindrome; // So that palindrome.erase doesn't wreck the origonal copy
     for(int i = 0; i <= palindrome.length(); i++) {
         palindrome = original;
         if(recognizer(palindrome.erase(i, 1))) { // Tests if the string is a palindrome without the Ith character
-            return palindrome;
+            // return palindrome;
+            break;
         }
     }
-    return "";
+    // return "";
 }
 
 
@@ -225,7 +233,7 @@ string decorruptionator(string palindrome) {
  * @returns     false       - the input can not be a palindrome
  *
  *****************************************************************************/
-bool detective(string palindrome) {
+bool detective(char palindrome[]) {
     // Count all letters, if all are even (except one), return true
     int oddCount = 0;
     char index = 'a';
@@ -258,7 +266,7 @@ bool detective(string palindrome) {
  * @returns     [string]    - the scrambled palindrome
  *
  *****************************************************************************/
-string cipher(string palindrome) {
+void cipher(char palindrome[]) {
     int rotations;
     cout << "Enter rotation distance (1-25): ";
     cin >> rotations;
@@ -268,7 +276,6 @@ string cipher(string palindrome) {
             palindrome.replace(i, 1, getPreviousLetter(palindrome.at(i)));
         }
     }
-    return palindrome;
 }
 
 
@@ -283,7 +290,7 @@ string cipher(string palindrome) {
  * @returns     [string]    - the unscrambled palindrome
  *
  *****************************************************************************/
-string decipher(string palindrome) {
+void decipher(char palindrome[]) {
     int rotations;
     cout << "Enter rotation distance (1-25): ";
     cin >> rotations;
@@ -293,8 +300,6 @@ string decipher(string palindrome) {
             palindrome.replace(i, 1, getNextLetter(palindrome.at(i)));
         }
     }
-    return palindrome;
-
 }
 
 
@@ -309,12 +314,11 @@ string decipher(string palindrome) {
  * @returns     [string]    - the new palindrome
  *
  *****************************************************************************/
-string toStandard(string palindrome) {
+void toStandard(char palindrome[]) {
     // Erases spaces, punctuation, and makes lowercase
     palindrome.erase(remove_if(palindrome.begin(), palindrome.end(), ::isspace), palindrome.end());
     palindrome.erase(remove_if(palindrome.begin(), palindrome.end(), ::ispunct), palindrome.end());
     transform(palindrome.begin(), palindrome.end(), palindrome.begin(), ::tolower);
-    return palindrome;
 }
 
 
@@ -327,11 +331,8 @@ string toStandard(string palindrome) {
  * @returns     [string]    - the line from the user, as a string
  *
  *****************************************************************************/
-string getInput() {
-    string input;
-    cin.ignore();
-    getline(cin, input); // Gets the whole line instead of one word
-    return input;
+void getInput(char palindrome[]) {
+    cin.getline(palindrome, 80); // Gets the whole line instead of one word
 }
 
 
@@ -347,64 +348,64 @@ string getInput() {
  * @returns     a           - error case
  *
  *****************************************************************************/
-string getNextLetter(char letter) {
+char getNextLetter(char letter) {
     // Hard-coded next letter
     switch(letter) {
         case 'a':
-            return "b";
+            return 'b';
         case 'b':
-            return "c";
+            return 'c';
         case 'c':
-            return "d";
+            return 'd';
         case 'd':
-            return "e";
+            return 'e';
         case 'e':
-            return "f";
+            return 'f';
         case 'f':
-            return "g";
+            return 'g';
         case 'g':
-            return "h";
+            return 'h';
         case 'h':
-            return "i";
+            return 'i';
         case 'i':
-            return "j";
+            return 'j';
         case 'j':
-            return "k";
+            return 'k';
         case 'k':
-            return "l";
+            return 'l';
         case 'l':
-            return "m";
+            return 'm';
         case 'm':
-            return "n";
+            return 'n';
         case 'n':
-            return "o";
+            return 'o';
         case 'o':
-            return "p";
+            return 'p';
         case 'p':
-            return "q";
+            return 'q';
         case 'q':
-            return "r";
+            return 'r';
         case 'r':
-            return "s";
+            return 's';
         case 's':
-            return "t";
+            return 't';
         case 't':
-            return "u";
+            return 'u';
         case 'u':
-            return "v";
+            return 'v';
         case 'v':
-            return "w";
+            return 'w';
         case 'w':
-            return "x";
+            return 'x';
         case 'x':
-            return "y";
+            return 'y';
         case 'y':
-            return "z";
+            return 'z';
         case 'z':
-            return "a";
+            return 'a';
         default: // Error case
             cout << "getNextLetter hit default" << endl;
-            return "a";
+            return 'a';
     }
 }
 
@@ -421,64 +422,64 @@ string getNextLetter(char letter) {
  * @returns     z           - error case
  *
  *****************************************************************************/
-string getPreviousLetter(char letter) {
+char getPreviousLetter(char letter) {
     // Hard-coded previous letter
     switch(letter) {
         case 'a':
-            return "z";
+            return 'z';
         case 'b':
-            return "a";
+            return 'a';
         case 'c':
-            return "b";
+            return 'b';
         case 'd':
-            return "c";
+            return 'c';
         case 'e':
-            return "d";
+            return 'd';
         case 'f':
-            return "e";
+            return 'e';
         case 'g':
-            return "f";
+            return 'f';
         case 'h':
-            return "g";
+            return 'g';
         case 'i':
-            return "h";
+            return 'h';
         case 'j':
-            return "i";
+            return 'i';
         case 'k':
-            return "j";
+            return 'j';
         case 'l':
-            return "k";
+            return 'k';
         case 'm':
-            return "l";
+            return 'l';
         case 'n':
-            return "m";
+            return 'm';
         case 'o':
-            return "n";
+            return 'n';
         case 'p':
-            return "o";
+            return 'o';
         case 'q':
-            return "p";
+            return 'p';
         case 'r':
-            return "q";
+            return 'q';
         case 's':
-            return "r";
+            return 'r';
         case 't':
-            return "s";
+            return 's';
         case 'u':
-            return "t";
+            return 't';
         case 'v':
-            return "u";
+            return 'u';
         case 'w':
-            return "v";
+            return 'v';
         case 'x':
-            return "w";
+            return 'w';
         case 'y':
-            return "x";
+            return 'x';
         case 'z':
-            return "y";
+            return 'y';
         default: // Error case
             cout << "getPreviousLetter hit default" << endl;
-            return "z";
+            return 'z';
     }
 
 }
