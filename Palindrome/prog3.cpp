e2d272dc1b3f7acfe62af0dc15453ff374d2b62a
Michael Pfeifer <mikepf97@gmail.com>
2015-10-23 08:53:00 -0600

    Continued converting to c style strings

--- Palindrome/prog3.cpp
+++ Palindrome/prog3.cpp
@@ -60,14 +60,14 @@
 using namespace std;
 
 // Prototypes
-bool recognizer         (char palindrome[]);
-void decorruptionator   (char palindrome[]);
-bool detective          (char palindrome[]);
-void cipher             (char palindrome[]);
-void decipher           (char palindrome[]);
+bool recognizer         (char palindrome[], int length);
+void decorruptionator   (char palindrome[], int length);
+bool detective          (char palindrome[], int length);
+void cipher             (char palindrome[], int length);
+void decipher           (char palindrome[], int length);
 
-void toStandard         (char palindrome[]);
-void getInput           (char palindrome[]);
+void toStandard         (char palindrome[], int length);
+int  getInput           (char palindrome[]);
 
 char getNextLetter      (char letter);
 char getPreviousLetter  (char letter);
@@ -87,6 +87,7 @@ int main() {
     bool quit = false;
     char choice;
     char phrase[81] = {'\0'};
+    int length;
 
     do {
         // Output main menu
@@ -107,23 +108,25 @@ int main() {
 
         // Break away based on selection
         switch(choice) {
+
             case '1': // Recognizer
                 cout << "Enter a string: ";
-                getInput(phrase);
-                if(recognizer(phrase)) {
+                length = getInput(phrase);
+                if(recognizer(phrase, length)) {
                     cout << phrase << " is a palindrome" << endl;
 
                 } else {
                     cout << phrase << " is not a palindrome" << endl;
                 }
                 break;
+
             case '2': // Decorruptionator
                 cout << "Enter a string: ";
-                getInput(phrase);
-                if(recognizer(phrase)) {
+                length = getInput(phrase);
+                if(recognizer(phrase, length)) {
                     cout << phrase << " is already a palindrome" << endl;
                 } else {
-                    decorruptionator(phrase);
+                    decorruptionator(phrase, length);
                     if(phrase != "") {
                         cout << "The decorrupted palindrome is " << phrase << endl;
                     } else {
@@ -131,38 +134,38 @@ int main() {
                     }
                 }
                 break;
+
             case '3': // Detective
                 cout << "Enter a string: ";
-                getInput(phrase);
-                if(detective(phrase)) {
+                length = getInput(phrase);
+                if(detective(phrase, length)) {
                     cout << "This can be a palindrome" << endl;
                 } else {
                     cout << "This can't be a palindrome" << endl;
                 }
                 break;
+
             case '4': // Cipher
                 cout << "Enter a string: ";
-                getInput(phrase);
-                if(recognizer(phrase)) {
-                    toStandard(phrase);
-                    cout << "Palindrome cipher complete: " << cipher(phrase) << endl;
-                } else {
-                    cout << phrase << " is not a palindrome" << endl;
-                }
+                length = getInput(phrase);
+                toStandard(phrase, length);
+                cipher(phrase, length);
+                cout << "Palindrome cipher complete: " << phrase << endl;
                 break;
+
             case '5': // Decipher
                 cout << "Enter a string: ";
-                getInput(phrase);
-                toStandard(phrase);
-                decipher(phrase);
+                length = getInput(phrase);
+                toStandard(phrase, length);
+                decipher(phrase, length);
                 cout << "Palindromic decipher complete: " << phrase << endl;
                 break;
 
-                break;
             case '6': // Quit
                 cout << "Goodbye!" << endl;
                 quit = true;
                 break;
+
             default: // Bad Input
                 cout << "Invalid option" << endl;
                 break;
@@ -184,11 +187,11 @@ int main() {
  * @returns     false       - the input was not a palindrome
  *
  *****************************************************************************/
-bool recognizer(char palindrome[]) {
-    palindrome = toStandard(palindrome);
-    for(int i = 0; i < palindrome.length() / 2; i++) {
+bool recognizer(char palindrome[], int length) {
+    toStandard(palindrome, length);
+    for(int i = 0; i < length / 2; i++) {
         // Tests if first equals last, 2nd equals 2nd to last, etc...
-        if(palindrome[i] != palindrome[palindrome.length() - 1 - i]) {
+        if(palindrome[i] != palindrome[length() - i]) {
             return false;
         }
     }
@@ -208,11 +211,11 @@ bool recognizer(char palindrome[]) {
  * @returns     [string]    - the input can be a palindrome, string contains a potential palindrome
  *
  *****************************************************************************/
-void decorruptionator(char palindrome[]) {
+void decorruptionator(char palindrome[], int length) {
     char[] original = palindrome; // So that palindrome.erase doesn't wreck the origonal copy
-    for(int i = 0; i <= palindrome.length(); i++) {
+    for(int i = 0; i <= length; i++) {
         palindrome = original;
-        if(recognizer(palindrome.erase(i, 1))) { // Tests if the string is a palindrome without the Ith character
+        if(recognizer(palindrome.erase(i, 1), length)) { // Tests if the string is a palindrome without the Ith character
             // return palindrome;
             break;
         }
@@ -233,14 +236,14 @@ void decorruptionator(char palindrome[]) {
  * @returns     false       - the input can not be a palindrome
  *
  *****************************************************************************/
-bool detective(char palindrome[]) {
+bool detective(char palindrome[], int length) {
     // Count all letters, if all are even (except one), return true
     int oddCount = 0;
     char index = 'a';
     for(int counter; counter < 26; counter++) {
         // Count instances of letter
         int instances = 0;
-        for(int i = 0; i < palindrome.length(); i++) {
+        for(int i = 0; i < length; i++) {
             if(index == palindrome[i]) {
                 instances++;
             }
@@ -266,12 +269,12 @@ bool detective(char palindrome[]) {
  * @returns     [string]    - the scrambled palindrome
  *
  *****************************************************************************/
-void cipher(char palindrome[]) {
+void cipher(char palindrome[], int length) {
     int rotations;
     cout << "Enter rotation distance (1-25): ";
     cin >> rotations;
     for(int j = 0; j < rotations; j++) {
-        for(int i = 0; i < palindrome.length(); i += 2) { // Counts by two to modify every other character
+        for(int i = 0; i < length; i += 2) { // Counts by two to modify every other character
             // Replaces i with the previous character j times
             palindrome.replace(i, 1, getPreviousLetter(palindrome.at(i)));
         }
@@ -290,12 +293,12 @@ void cipher(char palindrome[]) {
  * @returns     [string]    - the unscrambled palindrome
  *
  *****************************************************************************/
-void decipher(char palindrome[]) {
+void decipher(char palindrome[], int length) {
     int rotations;
     cout << "Enter rotation distance (1-25): ";
     cin >> rotations;
     for(int j = 0; j < rotations; j++) {
-        for(int i = 0; i < palindrome.length(); i += 2) { // Counts by two to modify every other character
+        for(int i = 0; i < length; i += 2) { // Counts by two to modify every other character
             // Replaces i with the next character j times
             palindrome.replace(i, 1, getNextLetter(palindrome.at(i)));
         }
@@ -314,7 +317,7 @@ void decipher(char palindrome[]) {
  * @returns     [string]    - the new palindrome
  *
  *****************************************************************************/
-void toStandard(char palindrome[]) {
+void toStandard(char palindrome[], int length) {
     // Erases spaces, punctuation, and makes lowercase
     palindrome.erase(remove_if(palindrome.begin(), palindrome.end(), ::isspace), palindrome.end());
     palindrome.erase(remove_if(palindrome.begin(), palindrome.end(), ::ispunct), palindrome.end());
@@ -331,7 +334,7 @@ void toStandard(char palindrome[]) {
  * @returns     [string]    - the line from the user, as a string
  *
  *****************************************************************************/
-void getInput(char palindrome[]) {
+int getInput(char palindrome[]) {
     cin.getline(palindrome, 80); // Gets the whole line instead of one word
 }
 
