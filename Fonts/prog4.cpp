984eb51d9adf364ec10fb1c76e3951e6e0d9d9f5
Michael Pfeifer <mikepf97@gmail.com>
2015-11-10 08:58:02 -0700

    Separated into printUsage method

--- Fonts/prog4.cpp
+++ Fonts/prog4.cpp
@@ -12,23 +12,17 @@ using namespace std;
 int  extractPointSize();
 void sortByCost();
 void readFontData();
+void readInputFile(ifstream& fin, char print[1001][1001], char fontName[1001][1001], int fontSize[1001], int index[1001]);
 int  getTonerUsed();
-void printUsage();
-void strrev(char str[]);
+void printUsage(char print[1001][1001], char fontName[1001][1001], int fontSize[1001], int toner[1001], int index[1001]);
 
 int main(int argc, char* argv[]) {
     // Variable Initilization
-    char records    [1001][1001]    = {'\0'};
-    char fontName   [1001][1001]    = {'\0'};
-    int  fontSize   [1001]          = {0};
-    int  toner      [1001]          = {0};
-    int  index      [1001]          = {0};
-
-    char inputChar  [1001]          = {'\0'};
-    int  inputInt                   = 0;
-
-    char fontChar   [1001]          = {'\0'};
-    char fontToner  [1001]          = {'\0'};
+    char print      [1001][1001]    = {'\0'}; // String to print
+    char fontName   [1001][1001]    = {'\0'}; // Name of font to use
+    int  fontSize   [1001]          = {0};    // Size of font
+    int  toner      [1001]          = {0};    // Total toner necessary
+    int  index      [1001]          = {0};    // Index of record
 
     int n = 3, i = 0, j = 0;
 
@@ -52,42 +46,58 @@ int main(int argc, char* argv[]) {
         return -1;
     }
 
-    i = 0;
+    readInputFile(fin, print, fontName, fontSize, index);
+    printUsage(print, fontName, fontSize, toner, index);
+
+    return 0;
+}
+
+int  extractPointSize();
+void sortByCost();
+void readFontData();
+
+void readInputFile(ifstream& fin, char print[1001][1001], char fontName[1001][1001], int fontSize[1001], int index[1001]) {
+    char inputChar[1001] = {'\0'};
+    int i = 0, j = 0;
     while(fin && fin.peek() != EOF) {
         // Get first line
         fin.getline(inputChar, 1001, '\n');
 
-        // Remove spaces
-        for(j = 0; j < strlen(inputChar); j++) {
-            if(inputChar[j] == ' ') {
-                inputChar[j] = '_';
-            }
-        }
+        // Enter record number
+        index[i] = i + 1;
 
         // Get font size
         int tempFont = 0;
-        i = strlen(inputChar) - 1;
+        j = strlen(inputChar) - 1;
         int fontLen = 0;
-        while(isdigit(inputChar[i - 1])) {
-            i--;
+        while(isdigit(inputChar[j - 1])) {
+            j--;
             fontLen++;
         }
         const char *fontArray = &inputChar[strlen(inputChar) - (fontLen + 1)];
         tempFont = atoi(fontArray);
-        cout << "Font Size: " << tempFont << endl;
+        fontSize[i] = tempFont;
 
         // Get font name
         inputChar[strlen(inputChar) - strlen(fontArray) - 1] = '\0';
-        cout << "Font name: " << inputChar << endl;
+        strcpy(fontName[i], inputChar);
 
         // Get second line
         fin.getline(inputChar, 1001, '\n');
-        strcpy(records[i], inputChar);
-
-        cout << endl;
+        strcpy(print[i], inputChar);
 
         i++;
     }
 
-    return 0;
+}
+
+int  getTonerUsed();
+
+void printUsage(char print[1001][1001], char fontName[1001][1001], int fontSize[1001], int toner[1001], int index[1001]) {
+    for(int i = 0; index[i] > 0; i++) {
+        cout << "Record: " << index[i] << "   Font: " << fontName[i] << "   Size: " << fontSize[i] << endl;
+        cout << "Toner required: " << toner[i] << endl;
+        cout << "String: " << print[i] << endl;
+        cout << endl;
+    }
 }
